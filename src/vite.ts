3d0b56403776ddcd927e946f3c865c6e96d08b9d
ikomom <yezm@zyaud.com>
2023-10-09 23:07:29 +0800

    test

--- src/vite.ts
+++ src/vite.ts
@@ -2,6 +2,12 @@ import { createHash } from 'crypto'
 import { Plugin } from 'vite'
 import { MiniwindRule } from './types'
 import { createGenerator, defaultRules } from '.'
+import * as fs from "fs";
+
+function appendLog(title: string, str: object) {
+  console.log(title, str)
+  // fs.appendFileSync('./t.log', `[${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}:${new Date().getMilliseconds()}] ${title} \n ${JSON.stringify(str)}\n\n` )
+}
 
 function getHash(input: string, length = 8) {
   return createHash('sha256')
@@ -19,7 +25,19 @@ export default function MiniwindVitePlugin(rules: MiniwindRule[] = defaultRules)
   return {
     name: 'miniwind',
     enforce: 'post',
+    options(options){
+      appendLog('options', {options})
+      return options
+    },
+    buildStart(options) {
+      appendLog('buildStart', {options})
+    },
+    buildEnd(err) {
+      appendLog('buildEnd', {err})
+    },
     transform(code, id) {
+      appendLog( 'transform', { id})
+
       if (id.endsWith('.css'))
         return null
 
@@ -29,12 +47,16 @@ export default function MiniwindVitePlugin(rules: MiniwindRule[] = defaultRules)
 
       const hash = getHash(id)
       map.set(hash, [id, style])
+      appendLog( 'transform:end', {id, style, code})
+
       return `import "${VIRTUAL_PREFIX}${hash}.css";${code}`
     },
     resolveId(id) {
+      appendLog( 'resolveId', { id})
       return id.startsWith(VIRTUAL_PREFIX) ? id : null
     },
     load(id) {
+      appendLog( 'load', { id})
       if (!id.startsWith(VIRTUAL_PREFIX))
         return null
 
@@ -42,8 +64,8 @@ export default function MiniwindVitePlugin(rules: MiniwindRule[] = defaultRules)
 
       const [source, css] = map.get(hash) || []
 
-      if (source)
-        this.addWatchFile(source)
+      // if (source)
+      //   this.addWatchFile(source)
 
       return css
     },
